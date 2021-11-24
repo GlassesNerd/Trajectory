d4da819e866fcdb0ef02741ac571a1bea1ad256c
GlassesNerd <glassesnerd@outlook.com>
2021-11-24 15:39:18 -0800

    change predefined input to user input

--- Trajectory/Source.cpp
+++ Trajectory/Source.cpp
@@ -8,9 +8,11 @@
 using namespace std;
 int main() {
 	ofstream cout("yeet.asy");
-	const double precision = 256, airDensity = 1.2, scale = 50;
-	long double vInit = 20, mass = 0.0027, angle = PI / 4.5, dragCoeff = 0.5, crossArea = 0.00125663706;
-	//cin>>vInit>>mass>>angle>>dragCoeff>> aCross;
+	const double precision = 256, airDensity = 1.2, scale = 50; 
+	//precision is how many times it calculates per second in the simulation
+	//scale is so that the labels for the start, end, and peak show up correctly
+	long double vInit, mass , angle , dragCoeff , crossArea ;
+	cin >> vInit >> mass >> angle >> dragCoeff >> crossArea;
 	long double x = 0, y = 0, vx = cos(angle) * vInit, vy = sin(angle) * vInit;
 	double prevx, prevy, maxx = 0, maxy = 0;
 	cout << "defaultpen(1);\n";
@@ -23,9 +25,9 @@ int main() {
 			maxy = y;
 			maxx = x;
 		}
-		//double slope = (y-prevy)/(x-prevx);
-		//double yint = y-slope*x;
-		//cout<<"y = "<<slope<<"x + "<<yint<<"{"<<prevx<<" <= x < "<<x<<"};\n";
+		/*double slope = (y-prevy)/(x-prevx);
+		double yint = y-slope*x;
+		cout<<"y = "<<slope<<"x + "<<yint<<"{"<<prevx<<" <= x < "<<x<<"};\n";*/
 		cout << "draw((" << scale * prevx << "," << scale * prevy << ")..(" << scale * x << "," << scale * y << "));\n";
 		vy -= g / precision;
 		const long double dragx = 0.5 * airDensity * vx * vx * dragCoeff * crossArea;
