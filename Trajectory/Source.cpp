caeccba7bd551715550b3e9679e1979d15433048
GlassesNerd <glassesnerd@outlook.com>
2021-11-29 19:59:33 -0800

    Add user prompts, errors for invalid values, and automatic scale for labels.

--- Trajectory/Source.cpp
+++ Trajectory/Source.cpp
@@ -1,44 +1,82 @@
 #include <iostream>
+#include <vector>
 #include <cmath>
 #include <math.h>
 #include <fstream>
 #include <iomanip>
+#include <algorithm>
 # define PI 3.14159265358979323846
 # define g 9.80665
 using namespace std;
 int main() {
-	ofstream cout("yeet.asy");
-	const double precision = 256, airDensity = 1.2, scale = 50; 
-	//precision is how many times it calculates per second in the simulation
-	//scale is so that the labels for the start, end, and peak show up correctly
-	long double vInit, mass , angle , dragCoeff , crossArea ;
-	cin >> vInit >> mass >> angle >> dragCoeff >> crossArea;
+	ofstream fout("yeet.asy");
+	const double precision = 256, airDensity = 1.2;
+	long double vInit, mass, angle, dragCoeff, crossArea;
+	cout << "Initial velocity of projectile: ";
+	cin >> vInit;
+	cout << "Mass of projectile: ";
+	cin >> mass;
+	cout << "Angle of launch (rad): ";
+	cin >> angle;
+	cout << "Drag coefficient of projectile: ";
+	cin >> dragCoeff;
+	cout << "Cross sectional area of projectile: ";
+	cin >> crossArea;
+
+	bool err = 0;
+	if (vInit <= 0) {
+		cout << "error: initial velocity must be positive\n";
+		err = 1;
+	}
+	if (mass <= 0) {
+		cout << "error: mass must be positive\n";
+		err = 1;
+	}
+	if (angle <= 0 || angle > 1.571) {
+		cout << "error: angle must be above 0 and less than pi/2\n";
+		err = 1;
+	}
+	if (dragCoeff < 0) {
+		cout << "error: drag coefficient must be nonnegative\n";
+		err = 1;
+	}
+	if (crossArea < 0) {
+		cout << "error: cross sectional area must be nonnegative\n";
+		err = 1;
+	}
+	if (err) return -1;
+
 	long double x = 0, y = 0, vx = cos(angle) * vInit, vy = sin(angle) * vInit;
-	double prevx, prevy, maxx = 0, maxy = 0;
-	cout << "defaultpen(1);\n";
+	long double prevx, prevy, peakx = 0, peaky = 0;
+	fout << "defaultpen(1);\n";
+	vector<pair<double, double>>coords = { {0,0} };
 	while (y >= 0) {
 		prevx = x;
 		prevy = y;
 		x += vx / precision;
 		y += vy / precision;
-		if (y > maxy) {
-			maxy = y;
-			maxx = x;
+		if (y > peaky) {
+			peaky = y;
+			peakx = x;
 		}
-		/*double slope = (y-prevy)/(x-prevx);
-		double yint = y-slope*x;
-		cout<<"y = "<<slope<<"x + "<<yint<<"{"<<prevx<<" <= x < "<<x<<"};\n";*/
-		cout << "draw((" << scale * prevx << "," << scale * prevy << ")..(" << scale * x << "," << scale * y << "));\n";
+		coords.push_back({ x,y });
 		vy -= g / precision;
 		const long double dragx = 0.5 * airDensity * vx * vx * dragCoeff * crossArea;
 		vx > 0 ? vx -= dragx / mass / precision : vx += dragx / mass / precision;
 		const long double dragy = 0.5 * airDensity * vy * vy * dragCoeff * crossArea;
 		vy > 0 ? vy -= dragy / mass / precision : vy += dragy / mass / precision;
 	}
-	cout << setprecision(3) << "label(\"start (0, 0)\", (0,0), SW);\n";
-	cout << setprecision(3) << "label(\"end (" << x << ", 0)\", " << "(" << scale * x << "," << scale * y << "), SE);\n";
-	cout << setprecision(3) << "label(\"peak (" << maxx << ", " << maxy << ")\", " << "(" << scale * maxx << "," << scale * maxy << "), N);\n";
-	cout << "dot((0, 0));\n";
-	cout << "dot((" << scale * x << ", " << scale * y << "));\n";
-	cout << "dot((" << scale * maxx << ", " << scale * maxy << "));\n";
+
+	double scale = 1000 / max(x, peaky);
+	for (int i = 1; i < coords.size(); i++) {
+		fout << "draw((" << scale * coords[i-1].first << "," << scale * coords[i-1].second << ")..(" << scale * coords[i].first << "," << scale * coords[i].second << "));\n";
+	}
+	fout.precision(3);
+	fout << "label(\"start (0, 0)\", (0,0), SW);\n";
+	fout << "label(\"end (" << x << ", 0)\", " << "(" << scale * x << "," << scale * y << "), SE);\n";
+	fout << "label(\"peak (" << peakx << ", " << peaky << ")\", " << "(" << scale * peakx << "," << scale * peaky << "), N);\n";
+	fout << "dot((0, 0));\n";
+	fout << "dot((" << scale * x << ", " << scale * y << "));\n";
+	fout << "dot((" << scale * peakx << ", " << scale * peaky << "));\n";
+	return 0;
 }
